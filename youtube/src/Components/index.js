42d51c316b0084d1b062873ea3534e5eba827302
Gavaskar <gavaskaraeg@gmail.com>
2023-11-15 20:30:49 +0000

    Set Layout

---
+++ youtube/src/Components/index.js
@@ -0,0 +1,9 @@
+import Navbar from "./Navbar";
+import Sidebar from "./Sidebar";
+import Thumnails from "./Thumnails";
+import MainContent from "./MainContent";
+export{
+    Navbar,
+    Sidebar,
+    MainContent
+}
